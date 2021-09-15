c2a42e458e1397c9aae035f2cd67d363f255be03
J.Gao <jacorygao@outlook.com>
2021-09-15 15:42:39 +0800

    Update batch_download.R

--- R/batch_download.R
+++ R/batch_download.R
@@ -12,26 +12,25 @@ library(utils)
 # user inputs
 # ==============================================================
 # txt file containing links
-txtFile <- "download_list_from_appears.txt"
+# txtFile <- "download_list_from_appears.txt"
 # output folder
-out_dir <- "Z:/Gao/"
+# out_dir <- "Z:/Gao/"
 # ==============================================================
 
+batch_download <- function(txtFile, out_dir) {
+    # links txt file
+    links <- read.table(txtFile, stringsAsFactors = FALSE)
 
-# links txt file
-links <- read.table(txtFile, stringsAsFactors = FALSE)
+    if(dir.exists(out_dir) == FALSE) dir.create(out_dir)
 
-if(dir.exists(out_dir) == FALSE) dir.create(out_dir)
-
-# download
-for (i in 1:nrow(links)) {
-    cur_link <- links[i, 1]
-    tmp <- unlist(strsplit(cur_link, "/"))
-    filename <- tmp[length(tmp)]
-    download.file(cur_link, file.path(out_dir, filename), method = "curl")
+    # download
+    for (i in 1:nrow(links)) {
+        cur_link <- links[i, 1]
+        tmp <- unlist(strsplit(cur_link, "/"))
+        filename <- tmp[length(tmp)]
+        download.file(cur_link, file.path(out_dir, filename), method = "curl")
+    }
 }
-
-
 
 
 
