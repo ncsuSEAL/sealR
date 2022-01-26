557402fc718883c8b9e0bb6f7f4b69688b151e9f
JGao <jacorygao@outlook.com>
2022-01-26 10:21:40 -0500

    Updated HLSv1.4 downloading function

--- R/download_hls.R
+++ R/download_hls.R
@@ -6,12 +6,28 @@
 library(utils)
 library(httr)
 library(XML)
+library(raster)
+library(rgdal)
+library(tools)
+library(gdalUtils)
 
-# example:
-# out_dir <- "Z:/Gao/"
-# tiles <- c("17SPC", "17SQC")
-# yrs <- c(2015, 2016, 2017, 2018, 2019)
-Download_HLS <- function(out_dir, tiles, yrs) {
+
+#' Download the HLS v1.4 image product. 
+#' @param out_dir Data save directory.
+#' @param tiles Tile names needed.
+#' @param yrs Years needed.
+#' @param crop_file A study region vector file path. If provided, the image will be croped to the specified region and saved to GTiff format.
+#' @return NULL
+#' @export
+#' @example 
+#' /dontrun {
+#' Download_HLS(
+#'     out_dir = "D:/Temp",
+#'     tiles = "15TYH", 
+#'     yrs = 2019, 
+#'     crop_file = "Q:/My Drive/Research/Urban_pheno/Data/ARD/madison_bbox.geojson")
+#' }
+Download_HLS <- function(out_dir, tiles, yrs, crop_file = NULL) {
     if(!dir.exists(out_dir)) dir.create(out_dir)
 
     root_url <- "https://hls.gsfc.nasa.gov/data/v1.4"
@@ -32,15 +48,38 @@ Download_HLS <- function(out_dir, tiles, yrs) {
                 data_urls <- paste0(tile_folder_url, "/", data_links)
                 # download
                 sapply(seq_along(data_urls), function(i) {
-                    if (!file.exists(file.path(out_dir, prod, data_links[i]))) {
-                         download.file(data_urls[i], file.path(out_dir, prod, data_links[i]), method = "curl")
+                    file_name <- file.path(out_dir, prod, data_links[i])
+                    if (!file.exists(file_name)) {
+                        download.file(data_urls[i], file_name, method = "curl")
+                        # crop file to study region
+                        if (!is.null(crop_file)) {
+                            # find out if the .hdf and .hdr files are all downloaded
+                            if (grepl(".hdr", file_name)) {
+                                hdr_file <- file_name
+                                hdf_file <- tools::file_path_sans_ext(file_name)
+                            } else {
+                                hdf_file <- file_name
+                                hdr_file <- paste0(file_name, ".hdr")
+                            }
+                            if (file.exists(hdf_file) & file.exists(hdr_file)) {
+                                sds <- get_subdatasets(hdf_file)
+                                r <- stack(sds)
+                                crop_extent <- readOGR(crop_file, verbos = FALSE)
+                                crop_extent_reproj <- spTransform(crop_extent, crs(r))
+                                r_crop <- raster::crop(r, crop_extent_reproj, snap = "in")
+                                writeRaster(r_crop, hdf_file, format = "GTiff", overwrite = TRUE)
+                                message(paste("croped file and saved it to GTiff: ", hdf_file))
+                                # remove the original hdf file
+                                file.remove(hdf_file)
+                                file.remove(hdr_file)
+                            }
+                        }
                     }
                 })
             }
         }
     }
 }
-
 
 
 
